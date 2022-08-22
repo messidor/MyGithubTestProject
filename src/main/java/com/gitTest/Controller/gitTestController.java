9de0c620c33aee72a25bf3b90b21eba5cee04cc9
alrdlrsha1 <alrdlrsha1@naver.com>
2022-08-22 20:18:19 +0900

    add controller

--- src/main/java/com/gitTest/Controller/gitTestController.java
+++ src/main/java/com/gitTest/Controller/gitTestController.java
@@ -1,2 +1,12 @@
-package com.gitTest.Controller;public class gitTestController {
+package com.gitTest.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class gitTestController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Todo Application!";
+    }
 }
